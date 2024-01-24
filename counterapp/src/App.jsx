5428928a2f52a69dd9c6c14c055507e3ccd46a8a
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-01-24 10:45:49 +0530

    Counter App

--- counterapp/src/App.jsx
+++ counterapp/src/App.jsx
@@ -8,7 +8,7 @@ function App( ) {
         <h1>Couter App</h1>
         <div className="my-5">
           <h2 className="my-5">{count}</h2>
-          <button className="btn btn-success mx-3 onC">Increment</button>
+          <button className="btn btn-success mx-3 onclick={}">Increment</button>
           <button className="btn btn-danger mx-3">Decrement</button>
             <button className="btn btn-secondary mx-3">Reset</button>
         </div>
